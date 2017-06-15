638639ea04055463285e38e462983885bfc49fe2
ankanroy007 <ankanroy007@users.noreply.github.com>
2017-06-15 07:24:48 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,12 @@
 def solution(num1, num2, end_num):
     """Enter Code Here"""
+    lis = []
+    i = 1
+    while i <= end_num:
+        if(i%num1 == 0 and i%num2 == 0):
+            lis.append(i)
+        i = i + 1
+
+    return lis
+
+solution(2, 3, 13)
