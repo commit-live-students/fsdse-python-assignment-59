dcfba3a53d5d04b62ca091146bab7f876fe1e00a
adinathauti <adinathauti@gmail.com>
2017-06-17 14:23:55 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,12 @@
 def solution(num1, num2, end_num):
-    """Enter Code Here"""
+    ls = []
+    i=1
+    nos = range(1,end_num+1)
+    while(i!=len(nos)):
+        if (i%num1==0)and(i%num2==0):
+            ls.append(i)
+        i=i+1
+    print ls
+    return ls
+#This does use a loop, but NOT a for-loop.
+solution(2,3,13)
