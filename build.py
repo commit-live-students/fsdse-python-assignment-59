516e854c0368d36c92129ab94325601ca2a5effb
datanomnom <ronzzz92@gmail.com>
2017-06-18 08:12:27 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,6 @@
-def solution(num1, num2, end_num):
-    """Enter Code Here"""
+def solution(num1, num2, num3):
+    final=[]
+    for num in range(1,num3):
+        if num%num1==0 and num%num2==0:
+            final.append(num)
+    return final
