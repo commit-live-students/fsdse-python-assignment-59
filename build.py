55891d822558ef76f4d0a883724237e3c569bf8e
arunabh15091989 <Arunabh.social@gmail.com>
2017-06-19 12:16:15 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,6 @@
 def solution(num1, num2, end_num):
     """Enter Code Here"""
+    test = list(range(1,end_num))
+    result = []
+    result = filter(lambda x: x%num1==0 and x%num2 == 0, test)
+    return result
