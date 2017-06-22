752a63faf334344a7a20f304a292cf3857018058
mudassirkhan19 <mudassirkhan19@users.noreply.github.com>
2017-06-22 07:34:50 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,19 @@
+def find_lcm(num1,num2):
+    greater = num1 if num1 > num2 else num2
+
+    while True:
+        if (greater % num1 == 0) and (greater % num2 == 0):
+            lcm = greater
+            break
+        greater += 1
+    return lcm
+
 def solution(num1, num2, end_num):
     """Enter Code Here"""
+    lcm = find_lcm(num1,num2)
+    check = lcm
+    l = []
+    while(check < end_num):
+        l.append(check)
+        check += lcm
+    return l
