47c24233ab3291ebcbc90f6a41e5200887b934a5
ramkrishan-sahu <ram04190@gmail.com>
2017-06-24 04:20:10 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,13 @@
-def solution(num1, num2, end_num):
-    """Enter Code Here"""
+x = 3
+y = 2
+a = 90
+
+def solution(no1, no2, up):
+    lis = []
+    for i in range (2, up+1):
+        if ((i % no1 == 0) & (i % no2 == 0)):
+            lis.append(i)
+    return lis
+
+solution(x, y, a)
+#NT DOne,
