bd2707c624b8e07723076c40f735db709fad8506
seekingpeace <seekingpeace@users.noreply.github.com>
2017-07-31 21:05:21 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,18 @@
+def lcm(x,y):
+    if x > y :
+        g=x
+    else:
+        g=y
+    while(True):
+        if((g%x==0) and (g%y==0)):
+            lcm=g
+            break
+        g+=1
+    return lcm
+
 def solution(num1, num2, end_num):
-    return [i for i in range(num1*num2,end_num,num1*num2)]
+    l=lcm(num1,num2)
+    return [i for i in range(num1*num2,end_num,l)]
 
 print(solution(2,3,24))
 
